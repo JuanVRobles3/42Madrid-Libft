5bd988af86916ae5847ed917e3696397c6e4fbbb
jrobles- <jrobles-@student.42madrid.com>
2022-02-04 14:23:47 +0100

    añado strtrim y strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -15,11 +15,22 @@
 char *ft_strjoin(char const *s1, char const *s2)
 {
     char *p;
-    size_t strlen;
+    size_t strlen1;
+    size_t strlen2;
+    int total;
 
     if (s1 == NULL || s2 == NULL)
         return (NULL);
-    strlen = ft_strlen(s1);
-    p = (char *) malloc (strlen * sizeof(char));
-
+    strlen1 = ft_strlen((char *)s1);
+    strlen2 = ft_strlen((char *)s2);
+    total = strlen1 + strlen2;
+    p = (char *) malloc ((total + 1) * sizeof(char));
+    //copiar el string 1 en la nueva cadena
+    //copiar el string 2 en la nueva cadena, solo que tiene que ser añadida al final de la 
+    //string 1 previamente copiada
+    if (p == NULL)
+        return (NULL);
+    ft_strlcpy(&p[0], s1, strlen1 + 1);
+    ft_strlcpy(&p[strlen1], s2, strlen2 + 1);
+    return (p);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,29 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrobles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/31 13:59:41 by jrobles-          #+#    #+#             */
-/*   Updated: 2022/02/02 19:59:32 by jrobles-         ###   ########.fr       */
+/*   Created: 2022/02/04 14:02:45 by jrobles-          #+#    #+#             */
+/*   Updated: 2022/02/04 14:02:46 by jrobles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+char *ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-
-	i = ft_strlen((char *)s);
-	if (c == 0)
-		return ((char *)s + i);
-	while (i >= 0)
-	{
-		if (s[i] == c)
-			return ((char *)s + i);
-		i--;
-	}
-	return (NULL);
+    size_t i;
+    size_t j;
+    
+    if (s1 == NULL)
+        return (NULL);
+    i = 0;
+	while (ft_strchr(set, s1[i]) && s1[i] != '\0')
+		i++;
+	j = ft_strlen((char *)s1);
+	while (ft_strchr(set, s1[j]) && j != 0)
+		j--;
+	if ((int)(j - i + 1) <= 0)
+		return (ft_calloc(1, 1));
+	return (ft_substr(s1, i, (j - i + 1)));
 }
